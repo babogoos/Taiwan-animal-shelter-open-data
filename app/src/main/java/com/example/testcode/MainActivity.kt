cb8a9df9f46c19e5ec34e0322a60afc77f0cfbd8
Dion <babogoos@gmail.com>
2021-02-02 22:50:52 +0800

    [Feature] Show error when get data fail.

--- app/src/main/java/com/example/testcode/MainActivity.kt
+++ app/src/main/java/com/example/testcode/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.testcode
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -44,6 +46,20 @@ class MainActivity : AppCompatActivity() {
     private fun initAdapter() {
         adapter = OpenDataAdapter()
         list.adapter = adapter
+
+        adapter.addLoadStateListener { loadState ->
+            val error = when {
+                loadState.prepend is LoadState.Error -> loadState.prepend as LoadState.Error
+                loadState.append is LoadState.Error -> loadState.append as LoadState.Error
+                loadState.refresh is LoadState.Error -> loadState.refresh as LoadState.Error
+                else -> null
+            }
+
+            error?.let {
+                Toast.makeText(this, it.error.message, Toast.LENGTH_LONG).show()
+            }
+        }
+
         lifecycleScope.launch {
             viewModel.getData().collectLatest {
                 adapter.submitData(it)
--- app/src/main/java/com/example/testcode/MainViewModel.kt
+++ app/src/main/java/com/example/testcode/MainViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.testcode
 
 import androidx.lifecycle.*
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.example.testcode.repository.IOpenDataRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
 import org.koin.java.KoinJavaComponent.inject
 
 
@@ -14,5 +17,8 @@ class MainViewModel : ViewModel() {
     private val openDataRepository: IOpenDataRepository by inject(IOpenDataRepository::class.java)
 
     @OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
-    suspend fun getData() = openDataRepository.getAnimalPlaceList()
+    suspend fun getData(): Flow<PagingData<OpenData>> {
+        return openDataRepository.getAnimalPlaceList()
+            .cachedIn(viewModelScope)
+    }
 }
