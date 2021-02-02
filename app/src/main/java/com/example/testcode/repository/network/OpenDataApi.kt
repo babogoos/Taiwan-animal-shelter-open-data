26fee737d85703eff64baa85c6a265107270e2b0
Dion <babogoos@gmail.com>
2021-02-02 16:52:24 +0800

    [Arch] Upgrade gradel, kotlin version.

--- app/src/main/java/com/example/testcode/repository/network/OpenDataApi.kt
+++ app/src/main/java/com/example/testcode/repository/network/OpenDataApi.kt
@@ -2,13 +2,16 @@ package com.example.testcode.repository.network
 
 import com.example.testcode.OpenData
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 /**
  * Created by dion on 2021/02/01.
  */
 interface OpenDataApi {
-    @GET("/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL")
-    suspend fun getOpenData() : List<OpenData>
+    @GET("/Service/OpenData/TransService.aspx")
+    suspend fun getOpenData(
+        @Query("UnitId") unitId: String = "QcbUEzN6E6DL"
+    ) : List<OpenData>
 
     companion object {
         fun create(): OpenDataApi = AppClientManager.retrofit.create(OpenDataApi::class.java)
